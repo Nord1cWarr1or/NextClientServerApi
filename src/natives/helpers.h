f346ab02cedfa85c95b44ebe4d9bf75a51c8ab25
Xelson <xelson1337@gmail.com>
2023-06-13 20:32:28 +0300

    Add silent return for ASSERT_ARG_IS_PLAYER for bots

--- src/natives/helpers.h
+++ src/natives/helpers.h
@@ -4,7 +4,10 @@
 
 #define PARAMS_COUNT (params[0] / sizeof(cell))
 #define ASSERT_ARG_IS_PLAYER(x)                                                                            \
-	if (params[x] <= 0 || params[x] > gpGlobals->maxClients) {                                             \
+	if (MF_IsPlayerBot(params[x])) {                                                                       \
+		return FALSE;                                                                                      \
+	}                                                                                                      \
+	if (!MF_IsPlayerValid(params[x])) {                                                                    \
 		MF_LogError(amx, AMX_ERR_NATIVE, "%s: invalid player index %i [%s]", __FUNCTION__, params[x], #x); \
 		return FALSE;                                                                                      \
 	}                                                                                                      \
@@ -15,7 +18,7 @@
 
 #define ASSERT_NO_NAPI_ERRORS()                                                         \
 	std::string _errorString;                                                           \
-	if (NAPIController()->ReadAndClearError(_errorString)) {                           \
+	if (NAPIController()->ReadAndClearError(_errorString)) {                            \
 		MF_LogError(amx, AMX_ERR_NATIVE, "%s: %s", __FUNCTION__, _errorString.c_str()); \
 		return FALSE;                                                                   \
 	}
